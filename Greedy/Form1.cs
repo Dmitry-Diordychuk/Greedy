41fe4ec16550958f987e87c6d0af6f22ccc89033
Diord <Diord@DESKTOP-6B09NPQ>
2019-02-27 11:20:05 +0300

    Add project files.

---
+++ Greedy/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Greedy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<PictureBox> vertices = new List<PictureBox>();
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //if(e.Button = Button)
+            PictureBox vertex = new PictureBox();
+            vertex.Location = new Point(e.X-10,e.Y-10);
+            vertex.SizeMode = PictureBoxSizeMode.StretchImage;
+            vertex.ClientSize = new Size(20, 20);
+            vertex.Image = new Bitmap(@"img\button.png");
+            this.Controls.Add(vertex);
+            vertices.Add(vertex);
+        }
+        void picturePoint_click(object sender, EventArgs e)
+        {
+            var pic = sender as PictureBox;
+            pic.Image = new Bitmap(@"img\buttonPressed.png");
+        }
+    }
+}
